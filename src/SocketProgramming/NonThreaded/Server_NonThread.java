31ae3e28e17252b4a29eb868032d7742277f3546
Sayan Shil <sayansil2000@gmail.com>
2024-11-10 13:04:54 +0530

    Update 1.0

--- src/SocketProgramming/NonThreaded/Server_NonThread.java
+++ src/SocketProgramming/NonThreaded/Server_NonThread.java
@@ -1,4 +1,4 @@
-package SocketProgramming;
+package SocketProgramming.NonThreaded;
 
 import java.io.IOException;
 import java.io.ObjectInput;
---
+++ src/SocketProgramming/Threaded/Server_Thread.java
@@ -0,0 +1,4 @@
+package SocketProgramming.Threaded;
+
+public class Server_Thread {
+}
