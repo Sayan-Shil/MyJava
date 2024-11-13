a670203f63150582bc122b32d0b4bb8c4afd1c02
Sayan Shil <sayansil2000@gmail.com>
2024-11-14 02:08:47 +0530

    Introducing Queue

---
+++ Java/Collection_Framework/Collection/List/ArrayList/Methods/Retrieval_Operations.java
@@ -0,0 +1,54 @@
+package Collection_Framework.Collection.List.ArrayList.Methods;
+
+import java.util.ArrayList;
+
+public class Retrieval_Operations {
+    public static ArrayList<Object> al= new ArrayList<>();
+    public static Object obj;
+    public static int index;
+
+    static void makeList(){
+        al.clear();
+        al.add("Sayan");
+        al.add("Shil");
+        al.add("Java");
+        al.add("Collections");
+        al.add("List");
+        al.add("ArrayList");
+        al.add(2);
+        al.add(497.59);
+    }
+
+    public static void main(String[] args) {
+        makeList();
+        System.out.println(al);
+
+        // Retrieve First object and 5th Object
+        obj = al.getFirst();
+        System.out.println(obj);
+
+        obj= al.get(4);
+        System.out.println(obj);
+
+        // retrieve the position where Java locates
+
+        index = al.indexOf("Java");
+        System.out.println(index);
+
+        al.add("Java");
+        System.out.println(al);
+        System.out.println(index);
+
+        index = al.lastIndexOf("Java");
+        System.out.println(index);
+
+        // retrieve and then replace
+
+        obj = al.set(7,500);
+        System.out.println(obj);
+        System.out.println(al);
+
+
+
+    }
+}
---
+++ Java/Collection_Framework/Collection/List/ArrayList/Methods/Sizing_Operations.java
@@ -0,0 +1,37 @@
+package Collection_Framework.Collection.List.ArrayList.Methods;
+
+import java.util.ArrayList;
+
+public class Sizing_Operations {
+    public static ArrayList<Object> al= new ArrayList<>();
+    public static int size;
+    public static boolean bool;
+
+    static void makeList(){
+        al.clear();
+        al.add("Sayan");
+        al.add("Shil");
+        al.add("Java");
+        al.add("Collections");
+        al.add("List");
+        al.add("ArrayList");
+        al.add(2);
+        al.add(497.59);
+    }
+
+    public static void main(String[] args) {
+        makeList();
+        System.out.println(al);
+
+        size=al.size();
+        System.out.println(size);
+
+        bool= al.isEmpty();
+        System.out.println(bool);
+
+        al.clear();
+        bool= al.isEmpty();
+        System.out.println(bool);
+
+    }
+}
